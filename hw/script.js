a288aef367d863afc0c3333e4deadd26c4c0f932
Viacheslav Pliusnin <via.plsn@gmail.com>
2021-02-20 15:25:50 +0200

    index.html, script.js, style.css files added. Adding program complete.

---
+++ hw/script.js
@@ -0,0 +1,25 @@
+alert('Програма для складання чисел: від числа "N" до числа "M", з можливістю пропускати парні числа.')
+
+let numberN = +prompt("Введіть, будь ласка, перше значення(має бути цілим числом).");
+let numberM = +prompt("Введіть, будь ласка, друге значення(має бути цілим числом).");
+
+if(Number.isInteger(numberN) && Number.isInteger(numberM)) {
+    let sum = 0;
+    let questionEven = confirm("Чи бажаєте пропускати парні числа в обрахунках?");
+    if(questionEven == true) {
+        for(let i = numberN; i <= numberM; i++){
+            if(i % 2 != 0) {
+                sum += i;
+            }
+        }
+        document.writeln(`<h1>Сума від ${numberN} до ${numberM}: ${sum}</h1>`);
+    } else {
+        for(let i = numberN; i <= numberM; i++){
+            sum += i;
+        }
+        document.writeln(`<h1>Сума від ${numberN} до ${numberM}: ${sum}</h1>`);
+    }
+} else {
+    alert("Помилка. Введені значення не є цілими, або не є числом.");
+    document.location.reload();
+}
